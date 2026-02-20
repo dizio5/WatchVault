4d8e1af4082fa499d35688e7e6196db6d19c923e
facuaavila5 <facundo.avila.027@mi.unc.edu.ar>
2026-02-20 02:03:34 -0300

    First commit

---
+++ src/main/java/com/dizio1/watchvault/WatchvaultApplication.java
@@ -0,0 +1,13 @@
+package com.dizio1.watchvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dizio1/watchvault/WatchvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizio1.watchvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
